164780f2f57165e0d02fac579265efae80f6f3b6
harry-lons <harrylonsdale25@gmail.com>
2024-11-12 19:59:16 -0800

    reworked model selection buttons into a select (dropdown)

--- frontend/src/3D.js
+++ frontend/src/3D.js
@@ -6,12 +6,18 @@ import STLModel from './models/STLModel'
 import ThorLabs from './models/ThorLabs'
 import MultiModel from './models/MultiModel'
 import PlanarGrid from './models/Grid';
+import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 function ThreeD() {
-    const [selectedModel, selectModelType] = useState(<div/>)
+    const [selectedModel, selectModelType] = useState("None")
 
     const selectModel = (model) => {
         selectModelType(model);
+    };
+
+    const handleChange = (event) => {
+        const key = event.target.value;
+        selectModelType(key);
     };
 
     const modelMap = {
@@ -20,7 +26,7 @@ function ThreeD() {
         'box': <OneBoxModel />,
         'STL': <STLModel />,
         'ThorLabs': <ThorLabs />,
-        'MultiModel': <MultiModel />,
+        'LabeledMulti1': <MultiModel />,
         'PlanarGrid': <PlanarGrid />
     }
 
@@ -29,16 +35,24 @@ function ThreeD() {
             <div className="intro-card">
                 <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
                     <p>Welcome to LabStream 3D. Select a model to view.</p>
-                    <div style={{ display: 'flex', flexDirection: 'row', gap: 5 }}>
-                        {Object.entries(modelMap).map(([key, value]) => (
-                            <button onClick={() => selectModel(value)}>{key}</button>
-                        ))}
-                    </div>
+                    <FormControl variant="outlined" sx={{ minWidth: 'auto', width: 'auto' }}>
+                        <InputLabel>Select a Model</InputLabel>
+                        <Select
+                            value={selectedModel}
+                            onChange={handleChange}
+                            label="Select a Model"
+                            sx={{ minWidth: 120, maxWidth: 200 }}
+                        >
+                            {Object.entries(modelMap).map(([key]) => (
+                                <MenuItem key={key} value={key}>{key}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                 </div>
             </div>
             {
                 // Display the selected model
-                selectedModel
+                modelMap[selectedModel]
             }
         </div>
     );
