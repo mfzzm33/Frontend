7e1c38bd5af18398207eefd8ac7486d4aa8f0fd5
mfzzm33 <145744591+mfzzm33@users.noreply.github.com>
2024-11-17 22:32:00 -0800

    Update App.js
    frontend integration with websocket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.scss';
+import {io} from "socket.io-client";
 import { Paper, Slider, TextField, Typography, Stack, Switch } from '@mui/material'
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid2'
 import ThreeD from './3D';
+import toast, {Toaster} from "react-hot-toast";
+
+const SOCKET_URL = (process.env.NODE_ENV == 'production') ? 'https://rangersw.com' : '127.0.0.1:8080';
 
 function App() {
+	const connecting = useRef(false);
+    	const socket_connection = useRef(null);
 	const [sliderInput, handleSliderUpdate] = useState({
         angle1: 0,
         angle2: 0,
@@ -15,6 +21,40 @@ function App() {
     });
 	const [is3D, switchDimensions] = useState(false)
 
+	useEffect(() => {
+        if(connecting.current) return;
+        connecting.current = true;
+        // This effect will run when the app starts
+        // this will be responsible for initializing the websocket connection
+        try {
+            console.log("attempting to connect")
+            const socket = io(SOCKET_URL, {
+                path: '/labstream/socket.io',
+            });
+
+            socket.on('connect', () => {
+                console.log('connected');
+                toast.success('Connected to the server',  {
+                    duration: 5000
+                });
+                socket_connection.current = socket;
+                socket.emit('gear_state');
+            })
+
+            // there is a new gear state, update the gear
+            socket.on('gear', (data) => {
+                // set_gear_state(data);
+				handleSliderUpdate({angle1 : data.gear1, angle2 : data.gear2, angle3 : data.gear3, angle3Input : data.gear3.toString()})
+                toast.success('Gear State has been retrieved or updated.',  {
+                    duration: 5000
+                });
+                console.log(data);
+            }, 200);
+        } catch(e) {
+            toast.error('Failed to connect to the server');
+        }
+    }, []);
+	
     const handleAngle3sliderUpdate = (value) => {
         handleSliderUpdate({ ...sliderInput, angle3: value, angle3Input: "" + value })
     }
@@ -39,6 +79,7 @@ function App() {
 
 	return (
 		<div className="TwoD-wrapper">
+			<Toaster position="bottom-left"  />
 			<Grid container spacing={2}>
 				<Grid size={3}>
 					<div className="intro-card">
@@ -77,7 +118,14 @@ function App() {
 								<Slider
 									valueLabelDisplay="on"
 									value={sliderInput.angle1}
-									onChange={(event, value) => handleSliderUpdate({...sliderInput, angle1: value })}
+									onChange={(event, value) => {
+										handleSliderUpdate({...sliderInput, angle1: value });
+										console.log("Sending gear1 to backend")
+										socket_connection.current.emit('adjust', {
+											gear: 1,
+											value: value
+										});
+									}}
 									min={-90}
 									max={90}
 								/>
@@ -86,7 +134,14 @@ function App() {
 							<div style={{ display: 'flex', flexDirection: 'row', marginTop: 10, width: '80%' }}>
 								<Slider
 									value={sliderInput.angle2}
-									onChange={(event, value) => handleSliderUpdate({...sliderInput, angle2: value })}
+									onChange={(event, value) => {
+										handleSliderUpdate({...sliderInput, angle2: value })
+										console.log("Sending gear2 to backend")
+										socket_connection.current.emit('adjust', {
+											gear: 2,
+											value: value
+										});
+									}}
 									min={-90}
 									max={90}
 								/>
@@ -96,14 +151,30 @@ function App() {
 							<div style={{ display: 'flex', flexDirection: 'row', marginTop: 10, width: '100%', gap: 5 }}>
 								<Slider
 									value={sliderInput.angle3}
-									onChange={(event, value) => handleAngle3sliderUpdate(value)}
+									onChange={(event, value) => {
+										handleAngle3sliderUpdate(value)
+										console.log("Sending gear3 to backend")
+										socket_connection.current.emit('adjust', {
+											gear: 3,
+											value: value
+										});
+									}}
 									min={-90}
 									max={90}
 								/>
 								<TextField
 									size="small"
 									value={sliderInput.angle3Input}
-									onChange={(event) => handleAngle3textUpdate(event.target.value)}
+									onChange={(event) => {
+										handleAngle3textUpdate(event.target.value)
+										console.log("Sending gear3 text to backend")
+										if(Number(event.target.value)){
+											socket_connection.current.emit('adjust', {
+												gear: 3,
+												value: Number(event.target.value)
+											});
+										}
+									}}
 									sx={{ width: 80 }}
 									inputProps={{ style: { textAlign: 'right' } }}
 								/>
